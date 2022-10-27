ea2275ecd5666765cd9bcd5c1480c0e0d186015e
AVS-2307 <andrey230781.an@gmail.com>
2022-10-27 14:59:16 +0300

    Initial Commit

---
+++ advertisements/views.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from django_filters import rest_framework as filters
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsAdvertisementOwner
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AdvertisementFilter
+
+    # настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action == "create":
+            return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsAdvertisementOwner()]
+        return []
